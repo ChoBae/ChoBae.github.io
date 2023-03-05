3ba497139266b7a854aadc12ad15c09ed547646d
Cho Sung Bae <101175828+ChoBaeKR@users.noreply.github.com>
2023-03-06 02:43:40 +0900

    test

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230306.023524';
+const cacheName = 'chirpy-20230306.024253';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
